9f37c5f2bc0f242f9c440f0d93618d14040c39cd
Jankin-Wu <2938567281@qq.com>
2025-09-21 11:45:24 +0800

    feat: Optimized filter box UI display

--- composeApp/src/commonMain/kotlin/com/jankinwu/fntv/client/ui/component/FilterBox.kt
+++ composeApp/src/commonMain/kotlin/com/jankinwu/fntv/client/ui/component/FilterBox.kt
@@ -21,8 +21,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateMapOf
@@ -48,7 +50,9 @@ import com.jankinwu.fntv.client.icons.RefreshCircle
 import com.jankinwu.fntv.client.viewmodel.UiState
 import io.github.composefluent.FluentTheme
 import io.github.composefluent.component.Icon
+import io.github.composefluent.component.ScrollbarContainer
 import io.github.composefluent.component.Text
+import io.github.composefluent.component.rememberScrollbarAdapter
 import io.github.composefluent.icons.Icons
 import io.github.composefluent.icons.regular.Dismiss
 
@@ -162,7 +166,7 @@ fun FilterChip(
     Row(
         modifier = Modifier
             .clip(CircleShape)
-            .background(FluentTheme.colors.background.card.default)
+            .background(FluentTheme.colors.background.card.tertiary)
             .border(1.dp, Color.Gray.copy(alpha = 0.4f), CircleShape)
             .clickable(
                 interactionSource = interactionSource,
@@ -225,65 +229,71 @@ fun FilterBox(
 //            onFilterBoxCollapse?.invoke(selectedOptions.toMap())
 //        }
 //    }
-
+    val scrollState = rememberScrollState()
     Box(
         modifier = modifier
             .background(FluentTheme.colors.background.card.default, RoundedCornerShape(8.dp))
     ) {
-        Column(
-            modifier = Modifier.padding(16.dp)
+        ScrollbarContainer(
+            adapter = rememberScrollbarAdapter(scrollState)
         ) {
-            // 遍历所有筛选组数据，为每一组创建一个FilterRow
-            dynamicFilterData.forEach { filterGroup ->
-                FilterRow(
-                    title = filterGroup.title,
-                    options = filterGroup.options,
-                    selectedOption = selectedOptions[filterGroup.title]
-                        ?: filterGroup.options.first(),
-                    onOptionSelected = { newSelection ->
-                        selectedOptions[filterGroup.title] = newSelection
-                        onFilterChanged(selectedOptions.toMap())
-                    }
-                )
-                Spacer(modifier = Modifier.height(16.dp)) // 行间距
-            }
-            Row(
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
+                    .padding(16.dp)
+                    .verticalScroll(scrollState)
             ) {
-                val interactionSource = remember { MutableInteractionSource() }
-                val isHovered by interactionSource.collectIsHoveredAsState()
+                // 遍历所有筛选组数据，为每一组创建一个FilterRow
+                dynamicFilterData.forEach { filterGroup ->
+                    FilterRow(
+                        title = filterGroup.title,
+                        options = filterGroup.options,
+                        selectedOption = selectedOptions[filterGroup.title]
+                            ?: filterGroup.options.first(),
+                        onOptionSelected = { newSelection ->
+                            selectedOptions[filterGroup.title] = newSelection
+                            onFilterChanged(selectedOptions.toMap())
+                        }
+                    )
+                    Spacer(modifier = Modifier.height(16.dp)) // 行间距
+                }
                 Row(
                     modifier = Modifier
-                        .clickable(
-                            interactionSource = interactionSource,
-                            indication = null, // 移除点击波纹效果
-                            onClick = { onFilterBoxCollapse?.invoke() }
-                        )
-                        .hoverable(interactionSource)
-                        .pointerHoverIcon(PointerIcon.Hand),
-                    horizontalArrangement = spacedBy(4.dp, Alignment.CenterHorizontally),
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Text(
-                        text = "收起",
-                        color = when {
-                            isHovered -> FluentTheme.colors.text.text.primary
-                            else -> FluentTheme.colors.text.text.tertiary
-                        },
-                        fontSize = 15.sp,
+                    val interactionSource = remember { MutableInteractionSource() }
+                    val isHovered by interactionSource.collectIsHoveredAsState()
+                    Row(
                         modifier = Modifier
+                            .clickable(
+                                interactionSource = interactionSource,
+                                indication = null, // 移除点击波纹效果
+                                onClick = { onFilterBoxCollapse?.invoke() }
+                            )
+                            .hoverable(interactionSource)
+                            .pointerHoverIcon(PointerIcon.Hand),
+                        horizontalArrangement = spacedBy(4.dp, Alignment.CenterHorizontally),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "收起",
+                            color = when {
+                                isHovered -> FluentTheme.colors.text.text.primary
+                                else -> FluentTheme.colors.text.text.tertiary
+                            },
+                            fontSize = 15.sp,
+                            modifier = Modifier
 
-                    )
-                    Icon(
-                        imageVector = DoubleArrowUp,
-                        contentDescription = "收起",
-                        tint = if (isHovered) FluentTheme.colors.text.text.primary else FluentTheme.colors.text.text.tertiary,
-                        modifier = Modifier
-                            .size(16.dp)
-                    )
+                        )
+                        Icon(
+                            imageVector = DoubleArrowUp,
+                            contentDescription = "收起",
+                            tint = if (isHovered) FluentTheme.colors.text.text.primary else FluentTheme.colors.text.text.tertiary,
+                            modifier = Modifier
+                                .size(16.dp)
+                        )
+                    }
                 }
             }
         }
@@ -315,7 +325,9 @@ fun FilterRow(
             text = title,
             color = FluentTheme.colors.text.text.tertiary,
             fontSize = 14.sp,
-            modifier = Modifier.padding(top = 4.dp) // 微调标题位置
+            modifier = Modifier
+                .width(90.dp)
+                .padding(top = 4.dp) // 微调标题位置
         )
         Spacer(modifier = Modifier.width(16.dp))
 
@@ -323,7 +335,7 @@ fun FilterRow(
         // 使用FlowRow可以在空间不足时自动换行
         FlowRow(
             modifier = Modifier.weight(1.0f),
-            horizontalArrangement = Arrangement.spacedBy(16.dp), // 选项之间的水平间距
+            horizontalArrangement = Arrangement.spacedBy(24.dp), // 选项之间的水平间距
             verticalArrangement = Arrangement.spacedBy(8.dp) // 换行后选项之间的垂直间距
         ) {
             options.forEach { option ->
@@ -478,6 +490,7 @@ private fun buildFilterData(
             val label = when (audioType) {
                 "DolbySurround" -> "杜比环绕"
                 "DolbyAtmos" -> "杜比全景声"
+                "Stereo" -> "立体声"
                 "Others" -> "其他"
                 else -> audioType
             }
